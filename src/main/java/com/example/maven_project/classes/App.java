8576b9cb4213d641bd87c649745d73610c8ef20b
nibrahim <nibrahim@integrant.com>
2023-11-22 14:11:05 +0200

    first commit

---
+++ src/main/java/com/example/maven_project/classes/App.java
@@ -0,0 +1,13 @@
+package com.example.maven_project.classes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is a Hello World Test !" );
+    }
+}
